130b649ba7dd74d52c1e4967d2f940313ced4e23
chsakell <chsakell@gmail.com>
2016-06-26 10:17:09 +0300

    rename app selector

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,7 +9,7 @@ import { APP_PROVIDERS } from './app.providers';
 import {SlimLoadingBar, SlimLoadingBarService} from 'ng2-slim-loading-bar/ng2-slim-loading-bar';
 
 @Component({
-    selector: 'app-container',
+    selector: 'scheduler',
     templateUrl: 'app/app.component.html',
     directives: [ROUTER_DIRECTIVES, SlimLoadingBar],
     providers: [APP_PROVIDERS]
