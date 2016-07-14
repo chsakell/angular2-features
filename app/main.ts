9ffb0c1b6235f5b7d0478b61df24772275bb9f71
chsakell <chsakell@gmail.com>
2016-07-14 20:45:49 +0300

    Added support for @angular/forms

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,14 @@
 ﻿import { bootstrap } from '@angular/platform-browser-dynamic';
+import {disableDeprecatedForms, provideForms} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 
-bootstrap(AppComponent, [APP_ROUTER_PROVIDERS]).then(
+bootstrap(AppComponent, [
+    APP_ROUTER_PROVIDERS,
+    disableDeprecatedForms(),
+    provideForms()
+]).then(
     success => console.log('AppComponent bootstrapped!'),
     error => console.log(error)
-);
+    );
